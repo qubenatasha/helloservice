60b3801cc8abc94472f5ce2b5b8d563a13312318
qubeship <noreply@qubeship.io>
2017-03-24 22:14:25 +0000

    initial commit

--- qube/src/api/swagger_models/response_messages.py
+++ qube/src/api/swagger_models/response_messages.py
@@ -1,8 +1,8 @@
 from flask_restful_swagger_2 import Schema
 
-from qube.src.api.swagger_models.hello import HelloErrorModel
-from qube.src.api.swagger_models.hello import HelloModel
-from qube.src.api.swagger_models.hello import HelloModelPostResponse
+from qube.src.api.swagger_models.helloservice import HelloServiceErrorModel
+from qube.src.api.swagger_models.helloservice import HelloServiceModel
+from qube.src.api.swagger_models.helloservice import HelloServiceModelPostResponse
 
 """
 the common response messages printed in swagger UI
@@ -11,7 +11,7 @@ the common response messages printed in swagger UI
 post_response_msgs = {
     '201': {
         'description': 'CREATED',
-        'schema': HelloModelPostResponse
+        'schema': HelloServiceModelPostResponse
     },
     '401': {
         'description': 'Unauthorized'
@@ -24,14 +24,14 @@ post_response_msgs = {
     },
     '500': {
         'description': 'Internal server error',
-        'schema': HelloErrorModel
+        'schema': HelloServiceErrorModel
     }
 }
 
 get_response_msgs = {
     '200': {
         'description': 'OK',
-        'schema': HelloModel
+        'schema': HelloServiceModel
     },
     '401': {
         'description': 'Unauthorized'
@@ -44,7 +44,7 @@ get_response_msgs = {
     },
     '500': {
         'description': 'Internal server error',
-        'schema': HelloErrorModel
+        'schema': HelloServiceErrorModel
     }
 }
 
@@ -63,7 +63,7 @@ put_response_msgs = {
     },
     '500': {
         'description': 'Internal server error',
-        'schema': HelloErrorModel
+        'schema': HelloServiceErrorModel
     }
 }
 
@@ -82,7 +82,7 @@ del_response_msgs = {
     },
     '500': {
         'description': 'Internal server error',
-        'schema': HelloErrorModel
+        'schema': HelloServiceErrorModel
     }
 }
 
